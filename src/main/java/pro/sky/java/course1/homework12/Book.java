5590c82932e9701ae188d216688ced35a3520c8a
madrahimova <darya.mad.ry@gmail.com>
2023-04-29 00:46:07 +0300

    Class methods added

--- src/main/java/pro/sky/java/course1/homework12/Book.java
+++ src/main/java/pro/sky/java/course1/homework12/Book.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.homework12;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
@@ -34,5 +36,18 @@ public class Book {
                 ", author=" + author +
                 ", year=" + year +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, year);
     }
 }
